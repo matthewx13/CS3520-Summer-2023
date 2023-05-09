ea5040f4cc041a4c6a120ff3c2c877b987b7f57c
Matthew <xue.ma@northeastern.edu>
2023-05-09 13:57:22 -0500

    Practice assignment 2

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// This is my entry for practice assignment 2 (Matthew Xue - 5/8)
+// The purpose of this code is to write my name Matthew Xue using symbols
+
+#include <iostream>
+using namespace std;
+int main() {
+  cout << " __   __  _______  _______  _______  __   __  _______  _     _    __   __  __   __  _______ " << endl;
+  cout << "|  |_|  ||   _   ||       ||       ||  | |  ||       || | _ | |  |  |_|  ||  | |  ||       |"<< endl;
+  cout << "|       ||  |_|  ||_     _||_     _||  |_|  ||    ___|| || || |  |       ||  | |  ||    ___|"<< endl;
+  cout << "|       ||       |  |   |    |   |  |       ||   |___ |       |  |       ||  |_|  ||   |___ "<< endl;
+  cout << "|       ||       |  |   |    |   |  |       ||    ___||       |   |     | |       ||    ___|"<< endl;
+  cout << "| ||_|| ||   _   |  |   |    |   |  |   _   ||   |___ |   _   |  |   _   ||       ||   |___ "<< endl;
+  cout << "|_|   |_||__| |__|  |___|    |___|  |__| |__||_______||__| |__|  |__| |__||_______||_______|"<< endl;
+}

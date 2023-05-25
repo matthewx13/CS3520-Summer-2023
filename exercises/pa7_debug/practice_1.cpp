4d1b303b120e0b76a1d2c0865d2747c454ad3755
Matthew <xue.ma@northeastern.edu>
2023-05-25 17:25:04 -0500

    Update practice_1.cpp

--- exercises/pa7_debug/practice_1.cpp
+++ exercises/pa7_debug/practice_1.cpp
@@ -1,4 +1,3 @@
-
 // Program to check if the user-provided number is a palindrome
 // Number of errors/bugs = 25
 
@@ -6,42 +5,55 @@
 //TODO: Fix the bugs and make sure the program runs as expected.
 //Provide a brief comment identifying each bug fixed.
 
+/* Issues that I am fixing
+- void main should be changed to int main
+- i changed input from a char to an int
+- changed the void function to a bool and named the function pal_func
+- remove the result argument from pal_func
+- add remainder varaible
+- fix small syntax issues in pal_func
+
+
+*/
 
 #include<iostream>
-void func(char x); 
-void main() {
-        char input = 1001;
-		  cout<<"enter an integer please"; 
-      returned_val = func2(return_value_function);
-        if (returned_val){ 
-                cout<<It is not a palindrome<<endl
+using namespace std;
+
+bool pal_func(int n); 
+
+int main() {
+      int input;
+		  cout<<"enter an integer please: "; 
+      cin >> input;
+      bool return_val = pal_func(input);
+        if (return_val){ 
+                cout<<"It is not a palindrome" <<endl; // added "" and ;
         }
         else{
                 cout<<"It is a palindrome"<<endl;
         }
 		
-		return 0
+		return 0; // missing ;
 }
 
 //the return value of your func should be true / false only
-void func(int n, double result)
+bool pal_func(int n) // removed result
 {
-            float given_number
-            int temp = 0; 
-            while (n = 0) {
-					remainder = n / 10;
-					n/10; 
-                    temp * 10 + remainder; 
-                        
-            }
+    int given_number = n;
+    int temp = 0;
+    int remainder; // added remainder
+    while (n != 0) { // change = to !=
+      remainder = n % 10; // change from / to % for remainder
+			n = n/10; // n =n/10 not just n/10
+      temp = temp * 10 + remainder; // set temp =
+    }
 
-            if (n == temp)  
-            {
-                    return false;
-            }
-            else
-            {
-                    return true;
-                }
+    if (given_number == temp) // change to given_number
+    {
+            return false;
+    }
+    else
+    {
+            return true; 
+        }
 }
-

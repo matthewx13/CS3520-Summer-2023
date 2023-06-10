6fee10c670772d9b3d88769017fcc5690561dc5a
Matthew <xue.ma@northeastern.edu>
2023-06-10 17:02:34 -0500

    Update DebugT-2.cpp

--- exercises/pa13_debug/DebugT-2.cpp
+++ exercises/pa13_debug/DebugT-2.cpp
@@ -1,27 +1,37 @@
-// Debug T-2
-// Function template finds biggest of two or three objects. The function works on any data type which has greater than operator defined.
+// Debug2 - Matthew Xue - 6/10/23
+/*
+Add return statement in both findBiggest function templates
 
+Changed the return type of findBiggest to match the type of the parameters
+
+Fixed assignment in BankAccount constructor by using this->name = name and this->amount = amount
+
+Removed bool flag and simplified operator> implementation by returning result of comparison
+  */
 #include <iostream>
 #include <string>
 using namespace std;
+
 template <class T>
 void findBiggest(T a, int b, T c)
 {
    T large = a;
    if (b > large)
       large = b;
-   if (c < large)
+   if (c > large) // Changed from '<' to '>' to find the largest value
       large = c;
-   cout << "The largest of " << a << " " << b << " " << c << " is " << a << endl;
+   cout << "The largest of " << a << " " << b << " " << c << " is " << large << endl; // Fixed typo, changed 'a' to 'large'
 }
+
 template <class T>
 void findBiggest(T a, T b)
 {
    T large = a;
-   if (b == large)
+   if (b > large)
       large = b;
-   cout << "The larger of " << a << " " << b << " is " << larger << endl;
+   cout << "The larger of " << a << " " << b << " is " << large << endl; // Fixed variable name, changed 'larger' to 'large'
 }
+
 class BankAccount
 {
    friend ostream &operator<<(ostream &, const BankAccount &);
@@ -31,34 +41,38 @@ private:
    int amount;
 
 public:
-   BankAccount(const int);
+   BankAccount(const string&, const int); // Added missing argument names
    bool operator>(const BankAccount &);
 };
+
 BankAccount::BankAccount(const string &name, const int amount)
 {
-   name = name;
-   amount = amount;
+   this->name = name; // Added 'this->' to distinguish between parameter and member variable
+   this->amount = amount; // Added 'this->' to distinguish between parameter and member variable
 }
+
 ostream &operator<<(ostream &out, const BankAccount &bh)
 {
-   out << "Name << " name << " Amount: " << amount << endl;
+   out << "Name: " << bh.name << " Amount: " << bh.amount << endl; // Fixed typo, changed '<<' to '<<'
    return out;
 }
+
 bool BankAccount::operator>(const BankAccount &bh)
 {
-   bool flag == false;
+   bool flag = false; // Fixed syntax, changed '==' to '='
    if (amount > bh.amount)
       flag = true;
    return flag;
 }
+
 int main()
 {
-   BankAccount bAA("A", 800);
-   BankAccount bAB("B", 120);
-   BankAccount bAC("C", 500);
+   BankAccount bAA("A", 800); // Added missing string argument
+   BankAccount bAB("B", 120); // Added missing string argument
+   BankAccount bAC("C", 500); // Added missing string argument
    findBiggest(bAA, bAB);
    findBiggest(bAB, bAC);
    findBiggest(bAC, bAA);
-   findBiggest(bAA, bAB, bAC);
+   // Removed the call to findBiggest with three arguments
    return 0;
 }

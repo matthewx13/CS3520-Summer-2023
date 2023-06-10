52043456989df6432fc9e422a938c88c073dc2bc
Matthew <xue.ma@northeastern.edu>
2023-06-10 17:19:09 -0500

    Update DebugT-3.cpp

--- exercises/pa13_debug/DebugT-3.cpp
+++ exercises/pa13_debug/DebugT-3.cpp
@@ -1,12 +1,17 @@
-// Debug T-3
+// Debug T-3 - Matthew Xue - 6/10/23
 // A container class for displaying a comma-separated list.
 // Check for possible compiler errors, logical errors and rectify them
 // Re-factor the code by adding few comments (make it readable) and
-// provide list of most important fixes (in comments)
-
+// provide a list of the most important fixes (in comments)
+/*
+- Fixed variable name, changed 'y' to 'x'
+- Declare and assign values to four kinds of CommaSeparatedLists
+- Calculate size and create CommaSeparatedList objects
+*/
 #include <iostream>
 #include <string>
 using namespace std;
+
 template <class T>
 class CommaSeparatedList
 {
@@ -18,25 +23,27 @@ public:
    CommaSeparatedList(T *d, int s);
    void printList();
 };
+
 template <class T>
-CommaSeparatedList<T>::CommaSeparatedList(T d, int s)
+CommaSeparatedList<T>::CommaSeparatedList(T *d, int s) // Fixed parameter name
 {
    data = d;
    size = s;
 }
+
 template <class T>
-    void CommaSeparatedList < T < ::printList()
+void CommaSeparatedList<T>::printList() // Added missing closing angle bracket
 {
-   cout >> "Comma separated list:" << endl;
+   cout << "Comma-separated list:" << endl; // Fixed operator, changed '>>' to '<<'
    for (int x = 0; x < size; ++x)
    {
-      cout << data[y];
+      cout << data[x]; // Fixed variable name, changed 'y' to 'x'
       if (x != size - 1)
          cout << ", ";
    }
-   cout << endl
-        << endl;
+   cout << endl << endl;
 }
+
 class Book
 {
    friend ostream &operator<<(ostream &, const Book &);
@@ -48,6 +55,7 @@ private:
 public:
    void setBook(string, int);
 };
+
 void Book::setBook(string Title, int pr)
 {
    title = Title;
@@ -56,8 +64,8 @@ void Book::setBook(string Title, int pr)
 
 ostream &operator<<(ostream &out, const Book &b)
 {
-   out << b.title << " was published in" << b.year;
-   return ostream;
+   out << b.title << " was published in " << b.year; // Added space after 'in'
+   return out;
 }
 
 class Customer
@@ -71,14 +79,16 @@ private:
 public:
    void setCustomer(string, double);
 };
+
 void Customer::setCustomer(string CustomerName, double pr)
 {
    name = CustomerName;
    balDue = pr;
 }
+
 ostream &operator<<(ostream &out, const Customer &aCustomer)
 {
-   out << aCustomer.name << " owes  $" << aCustomer.balDue;
+   out << aCustomer.name << " owes $" << aCustomer.balDue; // Added '$' before the balance amount
    return out;
 }
 
@@ -98,19 +108,22 @@ int main()
    someCustomers[3].setCustomer("Torrence", 123.99);
    someCustomers[4].setCustomer("Richard", 20.06);
    someCustomers[5].setCustomer("Curtin", 56999.19);
+
    // Calculate size and create CommaSeparatedList objects
    CommaSeparatedListSize = sizeof(someInts) / sizeof(someInts[0]);
    CommaSeparatedList<int> CommaSeparatedListOfIntegers(someInts, CommaSeparatedListSize);
    CommaSeparatedListSize = sizeof(someDoubles) / sizeof(someDoubles[0]);
    CommaSeparatedList<double> CommaSeparatedListOfDoubles(someDoubles, CommaSeparatedListSize);
-   CommaSeparatedListSize = sizeif(someBook) / sizeof(someBook[0]);
+   CommaSeparatedListSize = sizeof(someBook) / sizeof(someBook[0]); // Fixed typo, changed 'sizeif' to 'sizeof'
    CommaSeparatedList<Book> CommaSeparatedListOfBooks(someBook, CommaSeparatedListSize);
    CommaSeparatedListSize = sizeof(someCustomers) / sizeof(someCustomers[0]);
    CommaSeparatedList<Customer> CommaSeparatedListOfCustomers(someCustomers, CommaSeparatedListSize);
-   // Use showList() with each CommaSeparatedList
+
+   // Use printList() with each CommaSeparatedList
    CommaSeparatedListOfIntegers.printList();
    CommaSeparatedListOfDoubles.printList();
-   CommaSeparatedListOfBooks.prinList();
+   CommaSeparatedListOfBooks.printList(); // Fixed typo, changed 'prinList' to 'printList'
    CommaSeparatedListOfCustomers.printList();
+
    return 0;
 }

b8d8464bca4d606ff435d6773d4331bdf22502d0
Matthew <xue.ma@northeastern.edu>
2023-06-21 10:52:23 -0500

    Update ant.cpp

--- hw5_update/ant.cpp
+++ hw5_update/ant.cpp
@@ -6,121 +6,109 @@ Ant::Ant(int x, int y) : Organism(x, y), move_counter(0), starve_counter(0) {}
 Ant::~Ant() {}
 
 void Ant::move(World* world) {
-    // get the current position of the ant
-    int x = get_x();
-    int y = get_y();
+    int current_x = get_x();
+    int current_y = get_y();
 
-    // get the direction of the ant
     int direction = rand() % 8;
-    int new_x = x;
-    int new_y = y;
+    int new_x = current_x;
+    int new_y = current_y;
 
-    // calculate the new position of the ant, if it hits a wall, it will bounce like a ball
     switch (direction) {
-        case 0: // up
-            if (y - 1 < 0) {
-                new_y = y + 1;
+        case 0: // Move up
+            if (current_y - 1 < 0) {
+                new_y = current_y + 1;
             } else {
-                new_y = y - 1;
+                new_y = current_y - 1;
             }
             break;
-        case 1: // down
-            if (y + 1 >= world->get_world_size()) {
-                new_y = y - 1;
+        case 1: // Move down
+            if (current_y + 1 >= world->get_world_size()) {
+                new_y = current_y - 1;
             } else {
-                new_y = y + 1;
+                new_y = current_y + 1;
             }
             break;
-        case 2: // left
-            if (x - 1 < 0) {
-                new_x = x + 1;
+        case 2: // Move left
+            if (current_x - 1 < 0) {
+                new_x = current_x + 1;
             } else {
-                new_x = x - 1;
+                new_x = current_x - 1;
             }
             break;
-        case 3: // right
-            if (x + 1 >= world->get_world_size()) {
-                new_x = x - 1;
+        case 3: // Move right
+            if (current_x + 1 >= world->get_world_size()) {
+                new_x = current_x - 1;
             } else {
-                new_x = x + 1;
+                new_x = current_x + 1;
             }
             break;
-        case 4: // up-left
-            if (x - 1 < 0) {
-                new_x = x + 1;
+        case 4: // Move up-left
+            if (current_x - 1 < 0) {
+                new_x = current_x + 1;
             } else {
-                new_x = x - 1;
+                new_x = current_x - 1;
             }
-            if (y - 1 < 0) {
-                new_y = y + 1;
+            if (current_y - 1 < 0) {
+                new_y = current_y + 1;
             } else {
-                new_y = y - 1;
-            }
-            break;
-        case 5: // up-right
-            if (x + 1 >= world->get_world_size()) {
-                new_x = x - 1;
-            } else {
-                new_x = x + 1;
-            }
-            if (y - 1 < 0) {
-                new_y = y + 1;
-            } else {
-                new_y = y - 1;
+                new_y = current_y - 1;
             }
             break;
-        case 6: // down-left
-            if (x - 1 < 0) {
-                new_x = x + 1;
+        case 5: // Move up-right
+            if (current_x + 1 >= world->get_world_size()) {
+                new_x = current_x - 1;
             } else {
-                new_x = x - 1;
+                new_x = current_x + 1;
             }
-            if (y + 1 >= world->get_world_size()) {
-                new_y = y - 1;
+            if (current_y - 1 < 0) {
+                new_y = current_y + 1;
             } else {
-                new_y = y + 1;
+                new_y = current_y - 1;
             }
             break;
-        case 7: // down-right
-            if (x + 1 >= world->get_world_size()) {
-                new_x = x - 1;
+        case 6: // Move down-left
+            if (current_x - 1 < 0) {
+                new_x = current_x + 1;
             } else {
-                new_x = x + 1;
+                new_x = current_x - 1;
             }
-            if (y + 1 >= world->get_world_size()) {
-                new_y = y - 1;
+            if (current_y + 1 >= world->get_world_size()) {
+                new_y = current_y - 1;
             } else {
-                new_y = y + 1;
+                new_y = current_y + 1;
+            }
+            break;
+        case 7: // Move down-right
+            if (current_x + 1 >= world->get_world_size()) {
+                new_x = current_x - 1;
+            } else {
+                new_x = current_x + 1;
+            }
+            if (current_y + 1 >= world->get_world_size()) {
+                new_y = current_y - 1;
+            } else {
+                new_y = current_y + 1;
             }
             break;
     }
 
-    // if the new position is out of bounds, bounce the ant off the wall
-
-    // if the new position is unoccupied, move the ant to the new position
     if (world->is_empty(new_x, new_y)) {
-        // move the ant to the new position
         set_x(new_x);
         set_y(new_y);
 
-        // increment the move counter
         move_counter++;
         starve_counter = 0;
-    } else { // otherwise, increment the starve counter
-        // increment the starve counter
+    } else {
         starve_counter++;
-
         return;
     }
 }
 
 void Ant::breed(World* world) {
-    return; // ants do not breed
+    return; // Ants do not breed
 }
 
 void Ant::starve(World* world) {
-    // ants starve if they have not moved after 5 time steps
-    // if the ant has not moved after 5 time steps, it dies
     if (starve_counter >= 5) {
         world->remove_organism(this);
     }

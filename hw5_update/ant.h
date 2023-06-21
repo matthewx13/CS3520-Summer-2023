73bac3cc368b92901617d59d76cdafa07b73dcbe
Matthew <xue.ma@northeastern.edu>
2023-06-21 10:52:40 -0500

    Update ant.h

--- hw5_update/ant.h
+++ hw5_update/ant.h
@@ -3,11 +3,9 @@
 
 class Ant : public Organism {
 public:
-    // Constructor
     Ant(int x, int y);
     virtual ~Ant();
 
-    // Common methods for ants
     void move(World* world) override;
     void breed(World* world) override;
     void starve(World* world) override;

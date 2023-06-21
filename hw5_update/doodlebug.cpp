5e1566f7cef56442d58372fb712ed346acdb7f99
Matthew <xue.ma@northeastern.edu>
2023-06-21 12:02:36 -0500

    Update doodlebug.cpp

--- hw5_update/doodlebug.cpp
+++ hw5_update/doodlebug.cpp
@@ -7,34 +7,34 @@ Doodlebug::Doodlebug(int x, int y) : Organism(x, y), time_since_last_meal(0), mo
 Doodlebug::~Doodlebug() {}
 
 void Doodlebug::move(World* world) {
-    // moves like the Ant classes, except if there is an adjacent ant it will move to that position and eat the ant
-    // get the current position of the doodlebug
+    // movement of doodlebug and eats ants as well
+    // find the location
     int x = get_x();
     int y = get_y();
 
-    // check for adjacent ants
+
     if (is_adjacent_to_ant(world)) {
-        // if there is an adjacent ant, get the position of the ant
+        // if there is adjacent ant, find pos of any
         std::pair<int, int> ant_position = get_adjacent_ant(world);
 
-        // delete the ant
+        // ant removed
         world->remove_organism(world->get_organism(ant_position.first, ant_position.second));
 
-        // move the doodlebug to the position of the ant
+        // doodlebug goes toward ant
         set_x(ant_position.first);
         set_y(ant_position.second);
         move_counter++;
 
-        // increment the time since last meal
+        // time checker
         time_since_last_meal = 0;
-        
+
         return;
     } else {
-        // otherwise, move like the ant class
-        // get the direction of the doodlebug
+
+        // find direction that doodlebug is in
         int direction = rand() % 8;
 
-        // calculate the new position of the doodlebug
+        // find new pos
         int new_x = x;
         int new_y = y;
 
@@ -117,49 +117,49 @@ void Doodlebug::move(World* world) {
                 break;
         }
 
-        // if the new position is unoccupied, move the doodlebug to the new position
+        // doodlebug can move into unoccupied areas
         if (world->is_empty(new_x, new_y)) {
-            // move the doodlebug to the new position
+
             set_x(new_x);
             set_y(new_y);
 
-            // increment the time since last meal
+            // count time for when it ate last meal
             time_since_last_meal++;
             move_counter++;
         }
     }
 }
 
+// breeding
 void Doodlebug::breed(World* world) {
     if (move_counter >= 10) {
         create_new_doodlebug(world);
 
-        // reset the move counter
+       
         move_counter = 0;
     }
 }
 
 void Doodlebug::starve(World* world) {
     if (time_since_last_meal > 10) {
-        // remove the doodlebug from the world
+        // doodlebug death
         world->remove_organism(this);
     }
 }
-
 
 char Doodlebug::get_symbol() const {
     return 'X';
 }
 
 bool Doodlebug::is_adjacent_to_ant(World* world) {
-    // Get the current position of the doodlebug
+
     int x = get_x();
     int y = get_y();
 
-    // Define the possible relative positions of adjacent cells
+    // define possible pos of adjacent block
     const int adjacent_positions[][2] = {{-1, -1}, {0, -1}, {1, -1}, {-1, 0}, {1, 0}, {-1, 1}, {0, 1}, {1, 1}};
 
-    // Check for adjacent ants
+    // check for adjacent ants
     for (const auto& pos : adjacent_positions) {
         int new_x = x + pos[0];
         int new_y = y + pos[1];
@@ -172,14 +172,14 @@ bool Doodlebug::is_adjacent_to_ant(World* world) {
 }
 
 bool Doodlebug::is_ant(World* world, int x, int y) {
-    // Get organism at position
+
     if (x < 0 || x >= world->get_world_size() || y < 0 || y >= world->get_world_size()) {
         return false;
     }
 
     Organism* organism = world->get_organism(x, y);
 
-    // Check if organism is an ant (either Q, w, or o)
+    // check type
     if (organism != nullptr) {
         char symbol = organism->get_symbol();
         return symbol == 'Q' || symbol == 'w' || symbol == 'o';
@@ -189,11 +189,11 @@ bool Doodlebug::is_ant(World* world, int x, int y) {
 }
 
 std::pair<int, int> Doodlebug::get_adjacent_ant(World* world) {
-    // get the current position of the doodlebug
+
     int x = get_x();
     int y = get_y();
 
-    // check for adjacent ants
+
     if (is_ant(world, x - 1, y - 1)) {
         return std::make_pair(x - 1, y - 1);
     } else if (is_ant(world, x, y - 1)) {
@@ -216,11 +216,11 @@ std::pair<int, int> Doodlebug::get_adjacent_ant(World* world) {
 }
 
 void Doodlebug::create_new_doodlebug(World *world) {
-    // spawn a new doodlebug in a 16-adjacent neighborhood
+    // new doodlebug 
     int x = get_x();
     int y = get_y();
 
-    // find the empty cells in the 16-adjacency neighborhood
+    // find empty cells
     std::vector<std::pair<int, int>> empty_cells;
 
     for (int i = std::max(0, x - 2); i <= std::min(world->get_world_size() - 1, x + 2); i++) {
@@ -231,7 +231,7 @@ void Doodlebug::create_new_doodlebug(World *world) {
         }
     }
 
-    // if there are empty cells, spawn a new doodlebug in a random empty cell
+    // spawn doodlebug in empty cells
     if (!empty_cells.empty()) {
         int random_index = rand() % empty_cells.size();
         std::pair<int, int> random_empty_cell = empty_cells[random_index];

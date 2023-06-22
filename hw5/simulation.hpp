0b2818ec1857e0d6609f35d1579a097a0dc3a298
Matthew <xue.ma@northeastern.edu>
2023-06-22 11:24:48 -0500

    Delete hw5 directory

--- hw5/simulation.hpp
+++
@@ -1,9 +0,0 @@
-
-
-void game(int numDoodle, int numQueen, int numMale, int numWorker);
-
-void generate_points(int *bugx, int *bugy, int width, int height, int x_offset, int y_offset);
-
-
-
-enum State{INIT, ALIVE, DEAD, EXIT};

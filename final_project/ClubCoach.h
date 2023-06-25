1cad2cd6757c333f22d5b3fe32c41f302d6581ac
Matthew <xue.ma@northeastern.edu>
2023-06-25 16:51:26 -0500

    Update ClubCoach.h

--- final_project/ClubCoach.h
+++ final_project/ClubCoach.h
@@ -1,13 +1,19 @@
 #pragma once
 #include "User.h"
+using namespace std;
 
+// The ClubCoach class represents a coach in a club.
 class ClubCoach : public User {
 public:
-    ClubCoach(const std::string& username, const std::string& password);
+    // Constructor for the ClubCoach class.
+    ClubCoach(const string& username, const string& password);
 
+    // Checks if the coach can reserve a slot for a given reservation.
     bool can_reserve_slot(const Reservation& reservation) const override;
-    std::string& get_skill_level() override;
+
+    // Retrieves the skill level of the coach.
+    string& get_skill_level() override;
 
 private:
-    std::string skill_level_;
+    string skill_level_; // The skill level of the coach.
 };

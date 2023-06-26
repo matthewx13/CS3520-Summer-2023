1d074924e4fc25930021c97d20936340d3677b12
Matthew <xue.ma@northeastern.edu>
2023-06-26 11:47:25 -0500

    nvm it didnt work

--- final/ClubCoach.cpp
+++ final/ClubCoach.cpp
@@ -6,7 +6,7 @@ ClubCoach::ClubCoach(const std::string& username, const std::string& password)
 
 bool ClubCoach::can_reserve_slot(const Reservation& reservation) const {
     bool two_days = two_days_in_advance(reservation.get_start_time());
-    bool coach_hours = within_coach_hours(reservation.get_start_time(), reservation.get_end_time());
+    bool coach_hours = within_coach_hours(reservation.get_start_time()) && within_coach_hours(reservation.get_end_time());
     bool within_7_days = within_seven_days(reservation.get_start_time());
 
     return two_days && coach_hours && within_7_days;
